f08d238ced3fac01923a8c8d7ab0335637aa2cd7
nzrtnevada <rawogr@gmail.com>
2026-03-02 14:12:43 +0300

    Добавил представления в app catalog + urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, register_converter
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index),
+    path('<int:cat_id>/', views.categories),
+    path('<slug:cat_slug>/', views.categories_by_slug),
+    path('archive/<year4:year>/', views.archive),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Здесь вы найдете рецепты всего мира.")
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Рецепты по категориям</h1><p>id:{cat_id}</p>")
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Рецепты по категориям</h1><p>slug:{cat_slug}</p?")
+
+def archive(self, year):
+    return HttpResponse(f"<h1>Рецепты по годам</h1><p>{year}</p>")
