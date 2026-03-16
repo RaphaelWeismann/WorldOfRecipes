ae679943a18747af1a787b9c023035bd913e9510
nzrtnevada <rawogr@gmail.com>
2026-03-16 16:55:49 +0300

    Создал пользовательский менеджер для модели Recipe. Добавление перечисляемого поля для is_published

--- catalog/models.py
+++ catalog/models.py
@@ -2,13 +2,22 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class PublishedModel(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Recipe.Status.PUBLISHED)
+
 class Recipe(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, 
+                                       default=Status.DRAFT)
 
     class Meta:
         ordering = ['-time_create']
@@ -16,8 +25,11 @@ class Recipe(models.Model):
             models.Index(fields=['-time_create']),
         ]
 
-    def __str__(self):
-        return self.title
-    
+    objects = models.Manager()
+    published = PublishedModel
+
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+    def __str__(self):
+        return self.title
