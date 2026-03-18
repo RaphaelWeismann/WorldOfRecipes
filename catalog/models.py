8214b653d3a6913f343cf9c82097f004bb1740ee
nzrtnevada <rawogr@gmail.com>
2026-03-18 12:59:55 +0300

    Добавлена модель Category для рецептов

--- catalog/models.py
+++ catalog/models.py
@@ -18,6 +18,7 @@ class Recipe(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, 
                                        default=Status.DRAFT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     class Meta:
         ordering = ['-time_create']
@@ -33,3 +34,11 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+    
