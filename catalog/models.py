a3751796c4872c7425e6b30204c3820081711d58
nzrtnevada <rawogr@gmail.com>
2026-03-19 12:08:02 +0300

    Добавлена новая модель бд - TagPost. Добавлено поле tags в модели Recipe

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,9 @@ class Recipe(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, 
                                        default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name=
+                            'posts')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
 
     class Meta:
         ordering = ['-time_create']
@@ -45,3 +47,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
